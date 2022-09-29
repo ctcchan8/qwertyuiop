a6014e4deceb251f44d45cf880903c99839241fc
Charles Chan <ctcchan8-c@my.cityu.edu.hk>
2022-09-29 18:03:26 +0800

    habababababa

--- arduino_jen/arduino_jen.ino
+++ arduino_jen/arduino_jen.ino
@@ -1,5 +1,5 @@
-#include <esp32_can.h>
-#include <ESP32Servo.h>
+#include <Tasks.h>
+#include <due_can.h>
 
 #define INO_FILE
 
@@ -9,154 +9,134 @@
 #include "bounding_helper.hpp"
 #include "rm_motor.hpp"
 
+#define AIR_1A 3
+#define AIR_1B 2
+#define AIR_2A 4
+#define AIR_2B 5
 
-#define ELEVATOR 18
-#define CLAW_X 23
-#define CLAW_Y 25
-#define INTAKE 27
-
-Servo elevator;
-Servo claw_x;
-Servo claw_y;
-
-int elevator_pos;
-int claw_x_pos;
-int claw_y_pos;
+CAN_FRAME tx_msg, rx_msg;
 
 #define GROUP1_MOTOR_COUNT 4
 RMM3508Motor group1_rm[GROUP1_MOTOR_COUNT] = {
-  RMM3508Motor(0, DIRECT_OUTPUT_MODE),
-  RMM3508Motor(1, DIRECT_OUTPUT_MODE),
-  RMM3508Motor(2, DIRECT_OUTPUT_MODE),
-  RMM3508Motor(3, POS_TO_SPEED_PID_MODE)
-};
+  RMM3508Motor(0, SPEED_PID_MODE), 
+  RMM3508Motor(1, SPEED_PID_MODE),
+  RMM3508Motor(2, SPEED_PID_MODE),
+  RMM3508Motor(3, SPEED_PID_MODE)
+  };
 
-CAN_FRAME can_tx;
-
-// Task function, Task name, Stack size (bytes), Task parameter, Priority, Task handle, Core ID
-#define CREATE_ESP32_TASK(name) \
-  static TaskHandle_t name##_handle; \
-  xTaskCreatePinnedToCore(name, #name, 10000, NULL, 1, &name##_handle, 0);
-
-
-void setting_update_callback(JsonVariant var) {
-  JsonObject value = var.as<JsonObject>();
-
-  group1_rm[3].set_pos_pid(value["catapult"]["pos_pid"]);
-  group1_rm[3].set_speed_pid(value["catapult"]["speed_pid"]);
-}
-
-
-void drive_update_callback(JsonVariant var) {
-  JsonArray value = var.as<JsonArray>();
-
-  bool intake = value[0].as<bool>();
-  digitalWrite(INTAKE, intake ? HIGH : LOW);
-
-  elevator.write(value[1].as<int>());
-  claw_x.write(value[2].as<int>());
-  claw_y.write(value[3].as<int>());
+DECLARE_WATCHER(JsonObject, carbase_setting, "rc.s",
+  group1_rm[0].set_speed_pid(value["m0"]["pid"]);
+  group1_rm[1].set_speed_pid(value["m1"]["pid"]);
+  group1_rm[2].set_speed_pid(value["m2"]["pid"]);
+  group1_rm[3].set_speed_pid(value["m3"]["pid"]);
 
   static int count = 0;
+  console << "updated pid" << count++ << "\n";
+)
+
+DECLARE_WATCHER(JsonArray, gen_output, "rg.o",
+  bool air1 = value[0].as<bool>();
+  bool air2 = value[1].as<bool>();
+
+  digitalWrite(AIR_1A, air1 ? LOW : HIGH);
+  digitalWrite(AIR_1B, air1 ? HIGH : LOW);
+  digitalWrite(AIR_2A, air2 ? LOW : HIGH);
+  digitalWrite(AIR_2B, air2 ? HIGH : LOW);
+
+  static int count = 0;
+  // console << "updated " << count++ << "\n";
   console << count++ << "\n";
-}
+)
 
-void catapult_trigger_callback(JsonVariant var) {
-  int flag = var.as<int>();
-  
-  if (flag == 1 || flag == -1) {
-    if (group1_rm[3].output_mode != POS_PID_MODE) {
-      group1_rm[3].target_tick = group1_rm[3].unbound_tick;
-      // group1_rm[3].target_tick = 0;
-    } else {
-      group1_rm[3].target_tick -= 8192 * (3591/187.0);
-    }
-    group1_rm[3].output_mode = POS_PID_MODE;
-  } else if (flag == 2) {
-    group1_rm[3].target_speed = -1000;
-    group1_rm[3].speed_pid->_integral = 0;
-    group1_rm[3].speed_pid->_pre_error = 0;
-    group1_rm[3].output_mode = SPEED_PID_MODE;
-  } else {
-    group1_rm[3].output = 0;
-    group1_rm[3].output_mode = DIRECT_OUTPUT_MODE;
-  }
-}
+DECLARE_WATCHER(JsonArray, carbase_output, "rc.o",
+  int rm0 = value[0].as<int>();
+  int rm1 = value[1].as<int>();
+  int rm2 = value[2].as<int>();
+  int rm3 = value[3].as<int>();
 
-void can_callback(CAN_FRAME *frame) {
-  for (int i = 0; i < GROUP1_MOTOR_COUNT; i++) {
-    if (group1_rm[i].handle_packet(frame->id, frame->data.byte)) break;
-  }
-}
+  group1_rm[0].target_tick = rm0;
+  group1_rm[1].target_tick = rm1;
+  group1_rm[2].target_tick = rm2;
+  group1_rm[3].target_tick = rm3;
+
+  // static int count = 0;
+  // console << "shooter updated " << count++ << "\n";
+  // console << x << "\n";
+)
 
 void setup() {
-  pinMode(INTAKE, OUTPUT);
-  elevator.setPeriodHertz(50);
-  elevator.attach(ELEVATOR, 500, 2500);
-  elevator.write(170);
-  claw_x.setPeriodHertz(50);
-  claw_x.attach(CLAW_X, 500, 2500);
-  claw_x.write(40);
-  claw_y.setPeriodHertz(50);
-  claw_y.attach(CLAW_Y, 500, 2500);
-  claw_y.write(100);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(AIR_1A, OUTPUT);
+  pinMode(AIR_1A, OUTPUT);
+  pinMode(AIR_2B, OUTPUT);
+  pinMode(AIR_2B, OUTPUT);
 
-  gb.watch("setting", setting_update_callback);
-  gb.watch("drive", drive_update_callback);
-  gb.watch("catapult_trigger", catapult_trigger_callback);
+  START_WATCHER(carbase_setting);
+  START_WATCHER(gen_output);
+  START_WATCHER(carbase_output);
 
-  gb.setup(921600);
+  Can0.begin(CAN_BPS_1000K);  //  For communication with RM motors
 
-  CAN0.begin(1000000);
-  CAN0.watchFor();
-  CAN0.setCallback(0, can_callback);
+  gb.setup(115200);
 
-  CREATE_ESP32_TASK(sensor_feedback_task);
-  CREATE_ESP32_TASK(gb_loop_task);
+  Tasks_Add((Task)loop1, 1, 0);
+  Tasks_Add((Task)loop2, 10, 0);
+  Tasks_Add((Task)loop3, 1, 0);
+
+  // Start task scheduler
+  Tasks_Start();
 }
 
-void sensor_feedback_task(void * pvParameters) {
-  while (true) {
-    StaticJsonDocument<256> feedback;
-
-    // for (int i = 0; i < GROUP1_MOTOR_COUNT; i++) {
-    //   feedback[i * 3 + 0] = group1_rm[i].unbound_tick;
-    //   feedback[i * 3 + 1] = group1_rm[i].speed;
-    //   feedback[i * 3 + 2] = group1_rm[i].output;
-    // }
-
-    feedback[0] = group1_rm[3].unbound_tick;
-    feedback[1] = group1_rm[3].speed;
-    feedback[2] = group1_rm[3].output;
-
-    // for (int i = 0; i < GROUP1_MOTOR_COUNT; i++) {
-    //   feedback[i] = group1_rm[i].unbound_tick;
-    // }
-
-    gb.write("feedback", feedback);
-    
-    delay(20);
-  }
+void loop1() {  // Serial
+  gb.loop();
 }
 
-void gb_loop_task(void * pvParameters) {
-  while (true) {
-    gb.loop();
-    delay(1);
-  }
+void loop2() {  // Send sensors / encoders data
+  StaticJsonDocument<128> gen_feedback;
+  gen_feedback[0] = 123;
+  gen_feedback[1] = 456;
+
+  gb.write("rg.f", gen_feedback);
+
+  StaticJsonDocument<128> carbase_feedback;
+  carbase_feedback[0]  = group1_rm[0].unbound_tick;
+  carbase_feedback[1]  = group1_rm[0].speed;
+  carbase_feedback[2]  = group1_rm[0].output;
+
+  carbase_feedback[3]  = group1_rm[1].unbound_tick;
+  carbase_feedback[4]  = group1_rm[1].speed;
+  carbase_feedback[5]  = group1_rm[1].output;
+
+  carbase_feedback[6]  = group1_rm[2].unbound_tick;
+  carbase_feedback[7]  = group1_rm[2].speed;
+  carbase_feedback[8]  = group1_rm[2].output;
+
+  carbase_feedback[9]  = group1_rm[3].unbound_tick;
+  carbase_feedback[10] = group1_rm[3].speed;
+  carbase_feedback[11] = group1_rm[3].output;
+
+  gb.write("rc.f", carbase_feedback);
 }
 
-void loop() {
-  can_tx.id = 0x200;
-  can_tx.length = 8;
+void loop3() {  // PID Calculation
+  tx_msg.id = 0x200;
+  tx_msg.length = 8;
 
   for (int i = 0; i < GROUP1_MOTOR_COUNT; i++) {
     short output = group1_rm[i].get_output();
-    can_tx.data.byte[i * 2] = output >> 8;
-    can_tx.data.byte[i * 2 + 1] = output;
+    tx_msg.data.byte[i * 2] = output >> 8;
+    tx_msg.data.byte[i * 2 + 1] = output;
   }
-  
-  CAN0.sendFrame(can_tx);
 
-  delay(5);
+  Can0.sendFrame(tx_msg);
+}
+
+// the loop function runs over and over again forever, runs ~14000 times per second
+void loop() {
+  Can0.watchFor();
+  Can0.read(rx_msg);
+
+  for (int i = 0; i < GROUP1_MOTOR_COUNT; i++) {
+    if (group1_rm[i].handle_packet(rx_msg.id, rx_msg.data.byte)) break;
+  }
 }
--- workers/w1.py
+++ workers/w1.py
@@ -7,38 +7,41 @@ def run(worker: WorkerController):
     sm = worker.use_serial_manager()
     sm.whitelist.append(PortInfo(serial_number="5513132373735171A0B1", baudrate=115200))
     sm.whitelist.append(PortInfo(serial_number="7513131383235170F071", baudrate=115200))
-    sm.whitelist.append(PortInfo(serial_number="0001", baudrate=921600))
 
     gb.start_gateway(UDPBroadcast("255.255.255.255", 7986))
 
-    drive = gb.clone("drive")
-    catapult_trigger = 0
+    gen_output = gb.clone("rg.o")
+    carbease_output = gb.clone("rc.o")
 
     while True:
+        if isBtnJustPressed(RIGHT_L):
+            gen_output[0] = not gen_output[0]
 
-        drive[0] = isBtnPressing("kb:i")
+        if isBtnJustPressed(RIGHT_D):
+            gen_output[1] = not gen_output[1]
 
-        elevator_delta = (isBtnPressing("kb:o") - isBtnPressing("kb:l"))
-        claw_x_delta = (isBtnPressing("kb:Right") - isBtnPressing("kb:Left"))
-        claw_y_delta = (isBtnPressing("kb:Up") - isBtnPressing("kb:Down"))
+        gb.write("rg.o", list(gen_output))
 
-        drive[1] = max(80, min(drive[1] + elevator_delta, 170))
-        drive[2] = max(0, min(drive[2] + claw_x_delta, 140))
-        drive[3] = max(0, min(drive[3] + claw_y_delta, 110))
+        vx = getAxis(LEFT_X)
+        vy = getAxis(LEFT_Y)
+        A = getAxis(RIGHT_X)
 
-        if isBtnJustPressed("kb:g"):
-            catapult_trigger = 2
-        if isBtnJustReleased("kb:g"):
-            catapult_trigger = 1
-        if isBtnJustPressed("kb:f"):
-            if catapult_trigger != 1 and catapult_trigger != -1:
-                catapult_trigger = 1
-            catapult_trigger *= -1
+        # Calculate Speeds
+        vz = (vx + vy) * A
+        speed_multi = 2000
+        wheelspeed = [
+            (+ vx - vy - vz) * ( + speed_multi),
+            (- vx - vy + vz) * ( - speed_multi),
+            (- vx + vy - vz) * ( + speed_multi),
+            (+ vx + vy + vz) * ( - speed_multi)
+        ]
+        for i in range(4):
+            carbease_output[i] = wheelspeed[i]
 
-        gb.write("drive", list(drive))
-        gb.write("catapult_trigger", catapult_trigger)
+        gb.write("rc.o", list(carbease_output))
 
-        # print(drive)
-        # print("feedback", gb.read("feedback"))
+        ## print("local", gen_output)
+        ## ## print("e", time.perf_counter())
+        ## print("feedback", gb.read("rs.f"))
 
         worker.spin()
